a344346162f36b6928c80813cc7efec7ae87e0c9
pramod <pramod.mg@native5.com>
2014-07-02 18:21:37 +0530

    Changed most of the UI compoments espically the media queries which fit the resoultion of width from 320 to 445

--- platforms/android/assets/www/js/hover.js
+++ platforms/android/assets/www/js/hover.js
@@ -31,27 +31,59 @@ function move_circle()
 }
 
 function move_circle_1() {
-
+	
 	if(wi == 320)
 	{
-		$("#circle").animate({width :"570px",height :"570px",'margin-bottom':"0",left : "-23%"},"fast");
-		$(".condition").animate({top : "90px"},"fast");
+		$("#circle").animate({width :"500px",height :"500px",'margin-bottom':"0",left : "-17%"},"fast");
+		$(".icon1").css({position:"relative",left:"-1%"})
+		$(".condition").css({top : "21%"},"fast");
 		$("#circle").css({position : "relative"});
 		$("#cen").css({overflow :"hidden",position :"fixed"});
+		// $("#cir1").hide();
+		// $("#cir2").hide();
+		// $("#cir3").hide();
 	}
-    if (wi >= 350 && wi <= 480)
+    else if (wi >= 350 && wi <= 450)
     {
 		$("#circle").animate({width :"580px",height :"580px",'margin-bottom':"0",left : "-18%"},"fast");
 		$("#circle").css({position : "relative"});
 		$("#cen").css({overflow :"hidden",position :"fixed"});
+		// $("#cir1").hide();
+		// $("#cir2").hide();
+		// $("#cir3").hide();
+    }
+    else if(wi == 480)
+    {
+    	$("#circle").animate({width :"790px",height :"790px",left : "-155px"},"fast");
+		$(".icon1").css({position:"relative",left:"-1%"});
+		$("#circle").css({position : "relative"});
+		$("#cen").css({overflow : "hidden",position :"fixed"});	
+		// $("#cir1").hide();
+		// $("#cir2").hide();
+		// $("#cir3").hide();
     }
 
-    if (wi = 1280)
+    else if(wi >=446 && wi<=570)
     {
-		$("#circle").animate({width :"880px",height :"880px",'margin-bottom':"0",left : "30%"},"fast");
+    	$("#circle").animate({width :"830px",height :"830px",'margin-bottom' :"0",left : "-155px"},"fast");
+		$(".icon1").css({position:"relative",left:"-1%"});
 		$("#circle").css({position : "relative"});
-		$("#cen").css({position :"fixed"});
+		$("#cen").css({overflow : "hidden"});	
+		$("#cir1").hide();
+		$("#cir2").hide();
+		$("#cir3").hide();
     }
+
+  //   if (wi >= 451 && wi<=500)
+  //   {
+		// $("#circle").animate({width :"790px",height :"790px",left : "-155px"},"fast");
+		// $("#circle").css({position : "relative"});
+		// $("#cen").css({overflow : "hidden",position :"fixed"});
+		// // $("#cir1").hide();
+		// // $("#cir2").hide();
+		// // $("#cir3").hide();
+  //   }
+
     else {
         $("p.testp").text('Screen width is greater than 1200px. Width is currently: ' + wi + 'px.');
         }
@@ -59,18 +91,42 @@ function move_circle_1() {
 
 function move_cicle_1_back()
 {
-	if(wi == 320)
+	if(wi <= 320)
 	{
-		$("#circle").animate({width :"210px",height :"210px",'margin-bottom':"-80px",left :"0%"},"fast");
-		$(".condition").animate({top : "70px"},"fast");
+		$("#circle").animate({width :"220px",height :"220px",'margin-bottom':"-80px",left :"0%"},"fast");
 		$("#circle").css({position : "relative"});
-		$("#cen").css({overflow :"visible",position:"fixed"});
+		$(".icon1").css({position:"relative",left:"0%"})
+		$(".condition").css({top : "39%"},"fast");
+		$("#cen").css({overflow :"visible",position : "relative"});
+		// alert("here");
 	}
-    if (wi >= 350 && wi <= 480){
+
+    if (wi >= 350 && wi <= 450)
+    {
 		$("#circle").animate({width :"240px",height :"240px",'margin-bottom':"-80px",left : "0%"},"fast");
 		// $("#circle").css({position : "visible"});
 		$("#cen").css({overflow : "visible",position :"relative"});
-        }
+    }
+
+    if(wi === 480)
+    {
+		$(".condition").css({top : "60%"},"fast");
+    }
+
+    if(wi >=446 && wi<=570)
+    {
+    	$("#circle").animate({width :"320px",height :"320px",'margin-bottom' :"0",left : "0px"},"fast");
+		$(".icon1").css({position:"relative",left:"-1%"});
+		$("#circle").css({position : "relative"});
+		$("#cen").css({overflow : "hidden"});	
+    }
+
+    if (wi >= 451 && wi <= 500)
+    {
+		$("#circle").animate({width :"300px",height :"300px",left : "0%"},"fast");
+		// $("#circle").css({position : "visible"});
+		$("#cen").css({overflow : "visible",position :"relative"});
+    }
     else {
         $("p.testp").text('Screen width is greater than 1200px. Width is currently: ' + wi + 'px.');
         }
@@ -98,7 +154,6 @@ $("#cir3").on("click", function(evr) {
 
 $(".home-icon").on("click",function(){
 	move_circle_back();
-	move_cicle_1_back();
     $(".left1").hide();
     flag = true;
     $(".fa-tint").hide();
