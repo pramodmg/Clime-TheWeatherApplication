361843875b3843048ef22de8637e42a0bd96ed47
pramod <pramod.mg@native5.com>
2014-07-08 16:33:25 +0530

    Storing weather in the form of JSON object

--- www/js/demo.js
+++ www/js/demo.js
@@ -1,13 +1,11 @@
-    $(function() {      
-      //Keep track of how many swipes
-      var count=0;
-      //Enable swiping...
-      $("#test").swipe( {
-        //Generic swipe handler for all directions
-        swipeLeft:function(event, direction, distance, duration, fingerCount) {
-          $(this).text("You swiped " + direction + " times " );  
-        },
-        //Default is 75px, set to 0 for demo so any distance triggers swipe
-        threshold:0
-      });
-    });
+// $(document).on("click",function() {      
+    var element = document.getElementById('test');
+    alert("inside");
+    var count= 0 ;
+    Hammer(element).on("swipeleft", function(event) {
+      count++;
+        alert('hello! the count is ' + count);
+        });
+// });
+
+
